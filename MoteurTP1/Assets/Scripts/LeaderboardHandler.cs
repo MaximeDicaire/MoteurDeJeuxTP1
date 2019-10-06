2c730715d05f3ab8def9953c712cfafbcaf0eb8f
Jean-Simon tremblay <jean-simon_1997@hotmail.com>
2019-10-06 19:27:32 -0400

    still need to do insert but almost done :)

---
+++ MoteurTP1/Assets/Scripts/LeaderboardHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using SQLite4Unity3d;
+
+public class LeaderboardHandler : MonoBehaviour
+{
+    public Transform scrollview;
+    public GameObject prefab;
+
+    // Start is called before the first frame updatess
+    void OnEnable()
+    {
+        foreach (Transform child in scrollview)
+        {
+            Destroy(child.gameObject);
+        }
+
+        SQLiteConnection connection = new SQLiteConnection(Application.streamingAssetsPath + "/db.db", SQLiteOpenFlags.ReadWrite);
+        var scores = connection.Query<Scores>("SELECT * FROM Scores ORDER BY score DESC");
+
+        Scores[] scoresArr = scores.ToArray();
+        foreach(Scores score in scoresArr)
+        {
+            //Load la base de données et boucler les inscrutions pour chaque score.
+            GameObject scoreLine = Instantiate(prefab, scrollview);
+
+            Transform nameLabel = scoreLine.transform.Find("NameLabel");
+            TMP_Text nameLabelTextField = nameLabel.GetComponent<TMP_Text>();
+            nameLabelTextField.SetText(score.name);
+            
+            Transform scoreLabel = scoreLine.transform.Find("ScoreLabel");
+            TMP_Text scoreLabelTextField = scoreLabel.GetComponent<TMP_Text>();
+            scoreLabelTextField.text = score.score.ToString();
+        }
+
+    }
+}
---
+++ MoteurTP1/Assets/Scripts/Scores.cs
@@ -0,0 +1,9 @@
+﻿using SQLite4Unity3d;
+
+public class Scores
+{
+    [PrimaryKey, AutoIncrement]
+    public int id { get; set; }
+    public string name { get; set; }
+    public string score { get; set; }
+}
