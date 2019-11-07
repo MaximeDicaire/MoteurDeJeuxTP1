9b11b069ab2f85284a632414d5cbc60fd8480037
Frigh0st <maxime-81@hotmail.com>
2019-11-06 21:42:45 -0500

    Collision avec les collectibles et les murs (semi)

---
+++ MoteurTP1/Assets/Scripts/Controller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody body;
+    private bool murFrappe = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+
+        Vector3 movement = new Vector3();
+        movement.x += Input.GetAxis("Horizontal") * Time.fixedDeltaTime * moveSpeed;
+        movement.z += Input.GetAxis("Vertical") * Time.fixedDeltaTime * moveSpeed;
+
+        if (Physics.Raycast(transform.position,movement))
+        {
+            body.MovePosition(transform.position + movement);
+        }
+        
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            murFrappe = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            murFrappe = false;
+        }
+    }
+}
