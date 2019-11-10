8f7bd54a7e27a6ac305b6692b49bab7ed21083cb
Frigh0st <maxime-81@hotmail.com>
2019-11-10 14:53:05 -0500

    collectible finit

---
+++ MoteurTP1/Assets/CollectScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class CollectScript : MonoBehaviour
+{
+    public Rigidbody body;
+    public Collider monCollider;
+    public bool userTouch = false;
+    public int cpt = 0;
+    AudioSource audioCollectible;
+    public AudioClip collectible;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        audioCollectible = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(userTouch == true)
+        {
+                if(cpt <= 8)
+                {
+
+                    transform.localScale += new Vector3(0.5f, 0.5f, 0.5f);
+                    cpt++;
+
+                }
+                else if(cpt <= 16)
+                {
+
+                    transform.localScale -= new Vector3(0.5f, 0.5f, 0.5f);
+                cpt++;
+
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            monCollider.enabled = false;
+            userTouch = true;
+            audioCollectible.PlayOneShot(collectible, 1);
+        }
+    }
+
+}
---
+++ MoteurTP1/Assets/VerticalTurn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalTurn : MonoBehaviour
+{
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        // Spin the object around the world origin at 20 degrees/second.
+        transform.Rotate(new Vector3(0,Time.deltaTime*50,0));
+    }
+}
