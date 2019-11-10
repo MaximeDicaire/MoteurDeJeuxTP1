92ffeeb1ed8d39b9064a03c8aecd189511a8a1b2
Frigh0st <maxime-81@hotmail.com>
2019-11-10 13:08:44 -0500

    son marche

---
+++ MoteurTP1/Assets/unity-chan!/Unity-chan! Model/Scripts/SoundMarche.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(AudioSource))]
+
+public class SoundMarche : MonoBehaviour
+{
+    AudioSource audioMarche;        //son quand marche
+    public AudioClip marche;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Son
+        audioMarche = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Si on marche, on fait tourner le son
+        if (Input.GetKeyDown("up") || Input.GetKeyDown("down") || Input.GetKeyDown("right") || Input.GetKeyDown("left"))
+        {
+            audioMarche.Play();
+            audioMarche.loop = true;
+        }
+        //Si on marches pu on arrete le son
+        if (Input.GetKeyUp("up") || Input.GetKeyUp("down") || Input.GetKeyUp("right") || Input.GetKeyUp("left"))
+        {
+            audioMarche.Stop();
+            audioMarche.loop = false;
+        }
+    }
+}
