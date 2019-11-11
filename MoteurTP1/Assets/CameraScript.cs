1daa61bfccc9118a102defbfcef90452602ce055
Frigh0st <maxime-81@hotmail.com>
2019-11-11 10:34:38 -0500

    camera first person fonctionnelle

--- MoteurTP1/Assets/CameraScript.cs
+++ MoteurTP1/Assets/CameraScript.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
+    public Camera cam1, cam2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam1.enabled = true;
+        cam2.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            cam1.enabled = !cam1.enabled;
+            cam2.enabled = !cam2.enabled;
+        }
     }
 
 }
